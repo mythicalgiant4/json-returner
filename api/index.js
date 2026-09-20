ddbb0158eff70a6a2c91b8b0a07ef88277fd2517
mythicalgiant4 <mythicalgiant4@gmail.com>
2026-09-20 12:50:56 +0530

    initialization

---
+++ api/index.js
@@ -0,0 +1,23 @@
+export default function handler(req, res) {
+  // Enable CORS
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  // Handle OPTIONS request for CORS preflight
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
+  res.status(200).json({
+    "host": "192.168.31.127",
+    "relay": "",
+    "api": "",
+    "key": "vu4mQ7Gr0K3Hm2yqRsjissbD8lQc+NJO1OFTM87d0jA="
+  });
+}
