815300c4e6cbd8b528a8f8ecb5a8822f78b91ad6
mythicalgiant4 <mythicalgiant4@gmail.com>
2026-09-20 15:14:28 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ export default function handler(req, res) {
   }
   
   res.status(200).json({
-    "host": "192.168.31.127",
+    "host": "194.100.4.63",
     "relay": "",
     "api": "",
     "key": "vu4mQ7Gr0K3Hm2yqRsjissbD8lQc+NJO1OFTM87d0jA="
